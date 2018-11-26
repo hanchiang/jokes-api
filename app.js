d4bcf9f904702e7663025d8a5b60213bb4b772dd
Yap Han Chiang <yaphc@hotmail.com>
2018-11-26 22:42:09 +0800

    express helmet

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 const express = require('express');
+const helmet = require('helmet');
 
 const routes = require('./routes');
 const errorHandlers = require('./handlers/errorHandlers');
 const app = express();
 
-app.use(routes);
+app.use(helmet());
 
+app.use(routes);
 app.use(errorHandlers.notFound);
 
 if (process.env.NODE_ENV === 'production') {
