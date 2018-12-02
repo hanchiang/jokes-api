53e88cf1be07f6affa8417aaf255fc6fd3e66144
Yap Han Chiang <yaphc@hotmail.com>
2018-12-02 12:35:30 +0800

    run npm install in deploy

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,11 @@
 echo "Pulling from master..."
-
 git pull origin master
-
 echo "Successfully from master!"
 
+echo "Updating packages..."
+npm install
+echo "Packaged updated!"
+
 echo "Restarting server..."
-
 pm2 restart index
-
 echo "Server restarted Successfully!"
