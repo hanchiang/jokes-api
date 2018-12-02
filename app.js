2e8b3beb932e6d87c254e79d2e86ce5b49c2b79a
Yap Han Chiang <yaphc@hotmail.com>
2018-12-02 12:44:11 +0800

    fix ip log middleware not to use root directory

--- app.js
+++ app.js
@@ -38,7 +38,7 @@ app.post('/webhooks/github', (req, res) => {
 
 
 app.use(requestIp.mw());
-app.use('/', (req, res, next) => {
+app.use((req, res, next) => {
   logger.info(`IP: ${req.clientIp}, path: ${req.path}`);
 })
 
